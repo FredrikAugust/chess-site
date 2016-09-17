f6635c60bd736f1830a1bebc685bd64f57cf3a54
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-09-18 01:24:43 +0200

    changed some of the text on the site and msg in console

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,7 @@ $(document).ready(function () {
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
 });
+
+
+// hoi man
+console.log("Hey! Thanks for checking out the website.\nTo find more things created by me, go to:\nhttps://github.com/fredrikaugust");
